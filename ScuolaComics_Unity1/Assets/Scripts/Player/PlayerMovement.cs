4bc37333b7778bbe68b044f0be0cad5eb56ca999
Edoardo Del Sarto <e99delsarto@gmail.com>
2024-06-07 11:06:48 +0200

    Lezione 3 del 07/06/2024: + migliorato movimento camera

--- ScuolaComics_Unity1/Assets/Scripts/Player/PlayerMovement.cs
+++ ScuolaComics_Unity1/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,11 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float force = 100;
     [SerializeField] float rotationSensibility = 3;
+    [SerializeField] float verticalRotationSensibility = 1.5f;
+    [SerializeField] float horizontalRotationSensibility = 3;
     Camera _mainCamera;
     InputActionSystem inputActions;
     Rigidbody _rigidBody;
     Vector3 _direction;
     Vector2 _rotation;
+    private float horizontalRotation = 0f;
+    private float verticalRotation = 0f;
+    private float verticalAngularVelocity;
+    private float horizontalAngularVelocity;
+    private float rotationTime;
+
     private void Awake()
     {
         inputActions = new InputActionSystem();
@@ -27,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
         // inputActions.PlayerMovement.Right.canceled += RightCanceled;
 
         inputActions.PlayerRotation.MouseAxis.performed += MouseAxisPerformed;
+        inputActions.PlayerRotation.MouseAxis.canceled += MouseAxisPerformed;
 
         inputActions.PlayerMovementVector.Movement.performed += VectorMovementPerformed;
         // inputActions.PlayerMovementVector.Movement.canceled += VectorMovementCanceled;
@@ -41,7 +51,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void MouseAxisPerformed(InputAction.CallbackContext context)
     {
-        _rotation += context.ReadValue<Vector2>().normalized;
+        _rotation = context.ReadValue<Vector2>();
+
+        if(context.phase == InputActionPhase.Canceled)
+        {
+            Debug.Log("Mouse canceled");
+            _rotation = Vector2.zero;
+        }
     }
 
     private void ForwardCanceled(InputAction.CallbackContext obj)
@@ -118,7 +134,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void LateUpdate()
     {
-        _mainCamera.transform.localRotation = Quaternion.Euler(-_rotation.y * rotationSensibility, _rotation.x * rotationSensibility, 0); 
+        horizontalRotation += _rotation.x * horizontalRotationSensibility * Time.smoothDeltaTime;
+        verticalRotation += _rotation.y * verticalRotationSensibility * Time.smoothDeltaTime;
+
+        verticalRotation = math.clamp(verticalRotation, -45, 45);
+
+        _mainCamera.transform.localRotation = Quaternion.Euler(-verticalRotation, horizontalRotation, 0); 
         transform.rotation = Quaternion.Euler(0, _mainCamera.transform.rotation.eulerAngles.y, 0);
+
     }
+
+    // private void SmoothRotationUpdate()
+    // {
+
+    //     Debug.Log("Rotation " + _rotation);
+    //     // Calculate the vertical and horizontal rotation values based on mouse input
+    //     verticalRotation += _rotation.y * verticalRotationSensibility * Time.smoothDeltaTime * -1;
+    //     horizontalRotation += _rotation.x * verticalRotationSensibility * Time.smoothDeltaTime;
+
+    //     Debug.Log("Vertical " + verticalRotation);
+    //     Debug.Log("Horizontal " + horizontalRotation);
+
+    //     // Keep the vertical rotation between -90 and 90 degrees
+    //     verticalRotation = Mathf.Clamp(verticalRotation, -45, 45);
+
+    //     // Smoothly rotate the camera based on the calculated values
+    //     _mainCamera.transform.localRotation = Quaternion.Euler(
+    //         Mathf.SmoothDampAngle(transform.localEulerAngles.x, verticalRotation, ref verticalAngularVelocity, rotationTime), // Vertical rotation
+    //         Mathf.SmoothDampAngle(transform.localEulerAngles.y, horizontalRotation, ref horizontalAngularVelocity, rotationTime), // Horizontal rotation
+    //         0f); // Don't rotate around the Z axis
+    // }
 }
